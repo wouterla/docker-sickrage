12119574e62c80e72559ad5c6bc2a332822aba3b
Tim Haak <tim@haak.co.uk>
2015-01-25 13:21:55 +0200

    make cache survice restarts. addeds some files to the ignore

--- start.sh
+++ start.sh
@@ -1,18 +1,25 @@
 #! /bin/sh
 
-mkdir /config
+mkdir -p /config
+mkdir -p /config/cache
+
 cd /sickrage
 if [ -f /config/config.ini ]
 then
 	rm -rf /sickrage/config.ini
 	rm -rf /sickrage/sickbeard.db
+	rm -rf /sickrage/cache.db
 else
 	touch /config/config.ini
 	touch /config/sickbeard.db
+	cp -fr /sickrage/cache/* /config/cache/
+	touch /sickrage/cache.db
 #	mv -f /sickbeard/autoprocesstv/autoProcessTV.cfg /config/autoProcessTV.cfg
 fi
 ln -sf /config/config.ini /sickrage/config.ini
 ln -sf /config/sickbeard.db /sickrage/sickbeard.db
+ln -sf /config/cache /sickrage/cache
+ln -sf /sickrage/cache.db /sickrage/cache.db
 #ln -sf /config/autoProcessTV.cfg sickbeard/autoprocesstv/autoProcessTV.cfg
 
 /usr/bin/python SickBeard.py
