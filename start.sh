ebbf01ace88a15ee8e3cc30958414d9acea4b822
Tim Haak <tim@haak.co.uk>
2015-01-28 21:45:31 +0200

    somethings breaking something

--- start.sh
+++ start.sh
@@ -8,18 +8,41 @@ if [ -f /config/config.ini ]
 then
 	rm -rf /sickrage/config.ini
 	rm -rf /sickrage/sickbeard.db
-	rm -rf /sickrage/cache.db
+	rm -rf /config/sickbeard.db.v32
+	rm -rf /config/sickbeard.db.v33
+	rm -rf /config/sickbeard.db.v34
+	rm -rf /config/sickbeard.db.v35
+	rm -rf /config/sickbeard.db.v36
+	rm -rf /config/sickbeard.db.v37
+	rm -rf /config/sickbeard.db.v38
+	rm -rf /config/sickbeard.db.v39
+	rm -rf /config/sickbeard.db.v40
+	rm -rf /config/sickbeard.db.v41
+	rm -rf /config/sickbeard.db.v42
+	rm -rf /config/sickbeard.db.v43
+	rm -rf /config/sickbeard.db.v44
 else
 	touch /config/config.ini
 	touch /config/sickbeard.db
+	touch /config/sickbeard.db.v32
+	touch /config/sickbeard.db.v33
+	touch /config/sickbeard.db.v34
+	touch /config/sickbeard.db.v35
+	touch /config/sickbeard.db.v36
+	touch /config/sickbeard.db.v37
+	touch /config/sickbeard.db.v38
+	touch /config/sickbeard.db.v39
+	touch /config/sickbeard.db.v40
+	touch /config/sickbeard.db.v41
+	touch /config/sickbeard.db.v42
+	touch /config/sickbeard.db.v43
+	touch /config/sickbeard.db.v44
 	cp -fr /sickrage/cache/* /config/cache/
-	touch /sickrage/cache.db
 #	mv -f /sickbeard/autoprocesstv/autoProcessTV.cfg /config/autoProcessTV.cfg
 fi
 ln -sf /config/config.ini /sickrage/config.ini
-ln -sf /config/sickbeard.db /sickrage/sickbeard.db
+ln -sf /config/sickbeard.db* /sickrage/
 ln -sf /config/cache /sickrage/cache
-ln -sf /sickrage/cache.db /sickrage/cache.db
 #ln -sf /config/autoProcessTV.cfg sickbeard/autoprocesstv/autoProcessTV.cfg
 
 /usr/bin/python SickBeard.py
